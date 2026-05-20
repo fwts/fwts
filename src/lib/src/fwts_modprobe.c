fwts/fwts

commit e34d0663d94387e7f64b2641021692d964edc757

lib: modprobe: add checking the compressed zst module format
BugLink: https://bugs.launchpad.net/fwts/+bug/2077528

Newer kernel support modules compressed format zst, this make the
modules cannot be spotted correctly, add the zst checking for the
compressed modules.

Using __NR_finit_module syscall may result in an error=8(Exec format error)
So, using the modprobe command directly instead of relying on the
__NR_finit_module syscall.

Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_modprobe.c
@@ -182,12 +182,12 @@ int fwts_module_load(fwts_framework *fw, const char *module)
 	struct utsname u;
 	const size_t modlen = strlen(module);
 	char module_ko[modlen + 4];
+	char module_ko_zst[modlen + 8];
 	char path[PATH_MAX];
 	char modpath[PATH_MAX];
-	const char *params = "";
-	int fd;
 	bool loaded = false;
-
+	int status = 0;
+	char modeprobe_ko[64];
 	/*
 	 *  No need to unload if it's not already loaded
 	 */
@@ -207,26 +207,19 @@ int fwts_module_load(fwts_framework *fw, const char *module)
 	(void)snprintf(module_ko, sizeof(module_ko), "%s.ko", module);
 	(void)snprintf(modpath, sizeof(modpath), "/lib/modules/%s", u.release);
 	if (!fwts_module_find(module_ko, modpath, path, sizeof(path))) {
-		fwts_log_error(fw, "Cannot find module %s\n", module);
-		return FWTS_ERROR;
+		(void)snprintf(module_ko_zst, sizeof(module_ko_zst), "%s.ko.zst", module);
+		if (!fwts_module_find(module_ko_zst, modpath, path, sizeof(path))) {
+			fwts_log_error(fw, "Cannot find module %s\n", module);
+			return FWTS_ERROR;
+		}
 	}
 
-	/*
-	 *  We've found it, now try and load it
-	 */
-	fd = open(path, O_RDONLY);
-	if (fd < 0) {
-		fwts_log_error(fw, "Cannot open module %s, errno=%d (%s)\n",
-			path, errno, strerror(errno));
+	(void)snprintf(modeprobe_ko, sizeof("modprobe ") + modlen, "modprobe %s", module);
+	(void)fwts_exec(modeprobe_ko, &status);
+	if (status != FWTS_OK) {
+		fwts_log_error(fw, "modprobe module %s failed\n", module);
 		return FWTS_ERROR;
 	}
-	if (sys_finit_module(fd, params, 0) < 0) {
-		fwts_log_error(fw, "Cannot load module %s, errno=%d (%s)\n",
-			path, errno, strerror(errno));
-		(void)close(fd);
-		return FWTS_ERROR;
-	}
-	(void)close(fd);
 
 	return FWTS_OK;
 }
@@ -241,7 +234,9 @@ int fwts_module_load(fwts_framework *fw, const char *module)
 int fwts_module_unload(fwts_framework *fw, const char *module)
 {
 	bool loaded = false;
-	int ret;
+	char modeprobe_ko[64];
+	const size_t modlen = strlen(module);
+	int status = 0;
 
 	/*
 	 *  No need to unload if it's not already loaded
@@ -251,11 +246,12 @@ int fwts_module_unload(fwts_framework *fw, const char *module)
 			return FWTS_OK;
 	}
 
-	ret = sys_delete_module(module, O_NONBLOCK);
-	if (ret < 0) {
-		fwts_log_error(fw, "Cannot unload module %s, errno=%d (%s)\n",
-			module, errno, strerror(errno));
+	(void)snprintf(modeprobe_ko, sizeof("modprobe -r") + modlen, "modprobe -r %s", module);
+	(void)fwts_exec(modeprobe_ko, &status);
+	if (status != FWTS_OK) {
+		fwts_log_error(fw, "Cannot unload module %s\n", module);
 		return FWTS_ERROR;
 	}
+
 	return FWTS_OK;
 }

commit 10741944cfc725174965ab35d76bde1018298249

fwts_modprobe: Remove unused sys_finit_module() and sys_delete_module()
These functions are not used anymore since commit e34d0663d94387e7
("lib: modprobe: add checking the compressed zst module format").
Since they are marked as "static inline", Clang complains here:

 fwts_modprobe.c:118:19: error: unused function 'sys_finit_module'
  [-Werror,-Wunused-function]
   118 | static inline int sys_finit_module(
       |                   ^~~~~~~~~~~~~~~~
 fwts_modprobe.c:131:19: error: unused function 'sys_delete_module'
  [-Werror,-Wunused-function]
   131 | static inline int sys_delete_module(
       |                   ^~~~~~~~~~~~~~~~~

Thus let's simply delete these functions now to allow Clang to compile
this file without warnings.

Fixes: e34d0663 ("lib: modprobe: add checking the compressed zst module format")
Signed-off-by: Thomas Huth <thuth@redhat.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_modprobe.c
@@ -112,31 +112,6 @@ static bool fwts_module_find(
 }
 
 /*
- *  sys_finit_module()
- *	system call wrapper for finit_module
- */
-static inline int sys_finit_module(
-	int fd,
-	const char *param_values,
-	int flags)
-{
-	errno = 0;
-	return syscall(__NR_finit_module, fd, param_values, flags);
-}
-
-/*
- *  sys_delete_module()
- *	system call wrapper for delete_module
- */
-static inline int sys_delete_module(
-	const char *name,
-	int flags)
-{
-	errno = 0;
-	return syscall(__NR_delete_module, name, flags);
-}
-
-/*
  *  fwts_module_loaded()
  *	check if module is loaded, the name (without .ko suffix) is
  *	provided in string module. Boolean loaded is set to true if