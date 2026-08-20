ea47bd17614ee6550b73611e1486396407377c7c
Thomas Huth <thuth@redhat.com>
2026-08-20 14:21:23 +0200

    Replace the obsolete address of the FSF with an URL
    The FSF does not reside in the Franklin Street anymore. Replace the
    address with the URL, as it is suggested in the latest version of
    the GPLv2 at https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- src/cpu/virt/virt_vmx.c
+++ src/cpu/virt/virt_vmx.c
@@ -15,10 +15,7 @@
  * for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program in a file named COPYING; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
 
--- src/lib/src/fwts_json.c
+++ src/lib/src/fwts_json.c
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  *
  * This is a simplified json parser and json object store
  * based on json but does not support keywords and is designed
